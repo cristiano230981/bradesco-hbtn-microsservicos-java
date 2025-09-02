4be07d628dfa448592572477d018906e9faa8e44
Cristiano <cristiano@vbsolutions.com.br>
2025-09-02 12:24:08 -0300

    Criando o primeiro Microsserviço (endpoint) com SpringBoot usando Spring Tool Suite 4

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -8,7 +8,7 @@ public class MessageResource {
 
     @GetMapping("/simpleMessageWelcome")
     public String simpleMessageWelcome() {
-        return "BEM VINDO A AULA DE MICROSERVIÇO USANDO SPRING BOOT !!!";
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT";
     }
 
     @GetMapping("/login")
