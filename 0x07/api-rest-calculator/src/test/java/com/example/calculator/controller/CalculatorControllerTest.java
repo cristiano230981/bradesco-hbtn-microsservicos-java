e64a3424403f58db5171c89f0ef0cfde12bf4c55
Cristiano <cristiano@vbsolutions.com.br>
2025-10-03 10:36:47 -0300

    Construir uma API REST com Spring Boot com testes unitários em cada endpoint

--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
@@ -1,16 +1,17 @@
 package com.example.calculator.controller;
 
-import com.example.calculator.controller.CalculatorController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
 
@@ -19,8 +20,7 @@ public class CalculatorControllerTest {
 
     @Test
     void messageWelcome() throws Exception {
-        RequestBuilder request = get("/calculator/welcome");
-        MvcResult result = mvc.perform(request).andReturn();
+        MvcResult result = mvc.perform(get("/calculator/welcome")).andReturn();
         assertEquals("Bem vindo à CALCULATOR API REST.", result.getResponse().getContentAsString());
     }
 
@@ -31,6 +31,4 @@ public class CalculatorControllerTest {
                 .param("number2", "3")).andReturn();
         assertEquals("Resultado: 5.0", result.getResponse().getContentAsString());
     }
-
-    // Você pode seguir o mesmo padrão para os demais endpoints
 }
